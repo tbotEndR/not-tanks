e47328f41a6d0024bc98c015584962eb3201fdfe
tbotEndR <133337412+tbotEndR@users.noreply.github.com>
2024-01-13 21:11:55 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,129 @@
+#include "include/raylib.h"
+#include "include/raymath.h"
+#include <stdio.h>
+//----------------------------------------------------------------------------------
+// Local Variables Definition (local to this module)
+//----------------------------------------------------------------------------------
+Camera camera = { 0 };
+Vector3 cubePosition = { 0.0f, 1.0f, 0.0f };
+Ray mouseRay = { 0 };
+Vector3 mouseWorldPosition = { 0 };
+//  these plane vector values will make calculating intersections much easier
+Vector3 xzPlanePoint = { 0.0f, 1.0f, 0.0f };
+Vector3 xzPlaneVector1 = { 1.0f, 0.0f, 0.0f };
+Vector3 xzPlaneVector2 = { 0.0f, 0.0f, 1.0f };
+Vector3 xzNormalvector = { 1.0f, 0.0f, 1.0f };
+char buf1[50];
+char buf2[50];
+
+
+
+//----------------------------------------------------------------------------------
+// Local Functions Declaration
+//----------------------------------------------------------------------------------
+static void UpdateDrawFrame(void);          // Update and draw one frame
+Vector3 GetMouseWorldPosition(Ray *mouseRay);
+
+//----------------------------------------------------------------------------------
+// Main entry point
+//----------------------------------------------------------------------------------
+int main()
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
+
+    InitWindow(screenWidth, screenHeight, "-not-tanks!-");
+
+    camera.position = (Vector3){ 0.0f, 30.0f, -15.0f };
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    //--------------------------------------------------------------------------------------
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        if (IsKeyDown(KEY_D)) cubePosition.x += 1;
+        else if (IsKeyDown(KEY_A)) cubePosition.x -= 1;
+        if (IsKeyDown(KEY_W)) cubePosition.z += 1;
+        else if (IsKeyDown(KEY_S)) cubePosition.z -= 1;
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        {
+            mouseRay = GetMouseRay(GetMousePosition(), camera);
+            mouseWorldPosition = GetMouseWorldPosition(&mouseRay);
+        } 
+
+        sprintf(buf1, "FrameTime: %.2fms", GetFrameTime()*1000.0f);
+        UpdateDrawFrame();
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();                  // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
+
+// Update and draw game frame
+static void UpdateDrawFrame(void)
+{
+    // Update
+    //----------------------------------------------------------------------------------
+    //----------------------------------------------------------------------------------
+
+    // Draw
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        BeginMode3D(camera);
+
+            DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
+            DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
+            DrawGrid(50, 2.0f);
+
+        EndMode3D();
+
+        DrawText("This is a raylib example", 10, 40, 20, DARKGRAY);
+        DrawText(buf1, 10, 70, 20, DARKGRAY);
+        DrawText(buf2, 10, 100, 20, DARKGRAY);
+
+        DrawFPS(10, 10);
+
+    EndDrawing();
+    //----------------------------------------------------------------------------------
+}
+
+//  returns intersection of mouseRay and xzPlane
+Vector3 GetMouseWorldPosition(Ray *mouseRay)
+{
+    //  ((mouseRay.position) + t(mouseRay.direction)) = (xzPlanePoint) + r(xzPlaneVector1) + s(xzPlaneVector2)
+    //  aka. ((0 30 -15) + t(mouseRay.direction)) = (0 1 0) + r(1 0 0) + s(0 0 1)
+    float r = 0.0f, s = 0.0f, t = 0.0f;
+    Vector3 tmpMouseRayDirection = Vector3Scale(mouseRay->direction, -1.0);
+    Vector3 tmpPointVector = Vector3Subtract(mouseRay->position, xzPlanePoint);
+
+    //  (tmpPointVector) = r(1 0 0) + s(0 0 1) - t(tmpMouseRay.direction)
+    t = tmpPointVector.y / tmpMouseRayDirection.y;              	                    //  y values in other vectors are 0 so we can immediately solve for t
+    tmpMouseRayDirection = Vector3Scale(tmpMouseRayDirection, t);                       //  insert t
+    if (tmpMouseRayDirection.x >= 0) r = tmpPointVector.x - tmpMouseRayDirection.x;     //  solve for r
+    else r = tmpPointVector.x + abs(tmpMouseRayDirection.x);
+    if (tmpMouseRayDirection.z >= 0) s = tmpPointVector.z - tmpMouseRayDirection.z;     //  solve for s
+    else s = tmpPointVector.z + abs(tmpMouseRayDirection.z);
+
+    //  insert r and s into plane equation to find intersection
+    Vector3 planeVector1 = Vector3Scale(xzPlaneVector1, r);
+    Vector3 planeVector2 = Vector3Scale(xzPlaneVector2, s);
+    planeVector1 = Vector3Add(planeVector1, planeVector2);
+    Vector3 intersection = Vector3Add(xzPlanePoint, planeVector1);
+    return intersection;
+}
