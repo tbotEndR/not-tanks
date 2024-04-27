8142d8d081ee6ddfe0ca8b2242e2e0ade7856e5f
tbotEndR <thibaut.endress27@proton.me>
2024-04-27 22:04:59 +0200

    generic memory pool implementation

--- include/mempool.h
+++ include/mempool.h
@@ -1,20 +1,29 @@
 #ifndef MEMPOOL_H
 #define MEMPOOL_H
 
+typedef struct Stack_t {
+    int *stack;
+    int top;
+    int max;
+} Stack_t;
+
 typedef struct MemoryPool_t {
     void *pool;
-    void *nextFree;
-    void *arrEnd;
-    int actives;
-    int poolSize;
+    Stack_t *nextList;
     int blockSize;
+    int blocks;
 } MemoryPool_t;
 
 //  allocates a memory pool of n blocks of given blockSize
 MemoryPool_t *AllocPool(int blockSize, int n);
 void FreePool(MemoryPool_t *mp);
 void *AllocBlock(MemoryPool_t *mp);
-void FreeBlock(void *block);
-void UpdateNextFree(MemoryPool_t *mp);
+void FreeBlock(void *block, MemoryPool_t *mp);
+void PrintPool(MemoryPool_t *mp);
+Stack_t *StackCtor(int size);
+void StackDtor(Stack_t *stack);
+int Pop(Stack_t *stack);
+void Push(Stack_t *stack, int i);
+void PrintStack(Stack_t *stack);
 
 #endif
--- src/mempool.c
+++ src/mempool.c
@@ -1,51 +1,91 @@
 #include "mempool.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 MemoryPool_t *AllocPool(int blockSize, int n)
 {
     MemoryPool_t *newPool = (MemoryPool_t*) malloc(sizeof(MemoryPool_t));
+    newPool->pool = (void*) malloc(blockSize * n);
+    newPool->nextList = StackCtor(n);
     newPool->blockSize = blockSize;
-    newPool->poolSize = n * blockSize;
-    newPool->actives = 0;
-    newPool->pool = malloc(blockSize * n);
-    newPool->nextFree = newPool->pool;
-    newPool->arrEnd = newPool->pool + (n * blockSize);
+    newPool->blocks = n;
     return newPool;
 }
 
 void FreePool(MemoryPool_t *mp)
 {
     free(mp->pool);
+    StackDtor(mp->nextList);
     free(mp);
 }
 
 void *AllocBlock(MemoryPool_t *mp)
 {
-    if (mp->nextFree != NULL)
+    int nextFree = mp->nextList->stack[mp->nextList->top];
+    if (nextFree != -1)
     {
-        void *newBlock = mp->nextFree;
-        mp->actives++;
-        UpdateNextFree(mp);
+        void *newBlock = mp->pool + mp->blockSize * nextFree;
+        Pop(mp->nextList);
         return newBlock;
     }
 }
 
-void FreeBlock(void *block)
+void FreeBlock(void *block, MemoryPool_t *mp)
 {
-    block = NULL;
-
+    int blockIndex = (block - mp->pool) / mp->blockSize;
+    block = (void*) NULL;
+    Push(mp->nextList, blockIndex);
 }
 
-void UpdateNextFree(MemoryPool_t *mp)
+void PrintPool(MemoryPool_t *mp)
 {
-    do
+    for (int i = 0; i < mp->blocks; i++)
     {
-        (mp->nextFree)++;
-        if (mp->nextFree > mp->arrEnd) 
-        {
-            mp->nextFree = NULL;
-            break;
-        }
-            
-    } while (mp->nextFree != NULL);
+        printf("Block %d: [ 0x%x ]\n", i, mp->pool + mp->blockSize * i);
+    }
+}
+
+Stack_t *StackCtor(int size)
+{
+    Stack_t *newStack = (Stack_t*) malloc(sizeof(Stack_t));
+    newStack->stack = (int*) malloc(sizeof(int)*size);
+    for (int i = 0; i < size; i++)
+    {
+        newStack->stack[i] = i;
+    }
+    newStack->top = size-1;
+    newStack->max = size;
+    return newStack;
+}
+
+void StackDtor(Stack_t *stack)
+{
+    free(stack->stack);
+    free(stack);
+}
+
+int Pop(Stack_t *stack)
+{
+    if (stack->top < 0) return NULL;
+    int i = stack->stack[stack->top];
+    stack->stack[stack->top] = -1;
+    stack->top--;
+    return i;    
+}
+
+void Push(Stack_t *stack, int i)
+{
+    if ((stack->top < stack->max-1) && (stack->stack[stack->top]) != -1) 
+    {
+        stack->stack[++stack->top] = i;
+    }
+}
+
+void PrintStack(Stack_t *stack)
+{
+    for(int i = 0; i < stack->max; i++)
+    {
+        printf("[%d]:%d\n", i, stack->stack[i]);
+    }
+    printf("stack top: %d\n", stack->top);
 }
